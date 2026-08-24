916dd306bd45e9ddce1f1ba6208cd538377ba078
Lwazi Tech <lwazizuke105@gmail.com>
2026-08-25 00:44:51 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '*MR LZ*',
+  author: '‎',
+  botName: "*MR LWAZI-MD*",
+  botOwner: '*LWAZI*', // Your name
+  ownerNumber: '27640932215', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "" // Removed broken Knight Bot update URL
+};
+
+module.exports = settings;
